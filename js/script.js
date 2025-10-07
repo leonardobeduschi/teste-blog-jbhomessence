a33fcd92aee13974f8209636d49d4d437362aaef
Leonardo Beduschi Iunes <107271248+leonardobeduschi@users.noreply.github.com>
2025-10-07 15:58:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -482,7 +482,7 @@ if (window.location.pathname.includes("/blog/index.html") || window.location.pat
                     <p class="meta">${post.date} | ${post.category}</p>
                     <h2>${post.title}</h2>
                     <p class="excerpt">${truncatedExcerpt}</p>
-                    <a href="${post.id}/" class="read-more">
+                    <a href="post.html?id=${post.id}" class="read-more">
                       Leia mais <span class="sr-only">sobre ${post.title}</span>
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -602,3 +602,4 @@ if (window.location.pathname.includes("/blog/post.html")) {
     })
     .catch((err) => console.error("Erro ao carregar post:", err));
 }
+
